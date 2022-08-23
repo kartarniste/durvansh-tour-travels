cec93d04cd4b31b196340443f150d756ac30d0d3
kartarniste <kkniste@gmail.com>
2022-08-23 10:11:34 +0530

    Update InvoiceTotalInfo.js

--- src/CustomComponent/invoice/InvoiceTotal/InvoiceTotalInfo.js
+++ src/CustomComponent/invoice/InvoiceTotal/InvoiceTotalInfo.js
@@ -34,13 +34,12 @@ function InvoiceTotalInfo({invoiceData}){
       }
 
       const numberWithCommas = (num) => {
-         var value =  num ? num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '';
-         if(!isNaN(parseFloat(value))){
-            value =  parseFloat(value);
-            value = value.toFixed(2);
-         }
-         return value;
-      }
+       if(!isNaN(parseFloat(num))){
+          num =  parseFloat(num);
+          num = num.toFixed(2);
+       }
+       return  num ? num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') : '';
+     }
 
     return (
         <View className="vbox">
